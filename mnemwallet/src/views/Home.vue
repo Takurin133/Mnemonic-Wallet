2d62c703567a617761a868badbc6995ba07f6031
LAPTOP-17H79KHE\taku1 <taku.133@icloud.com>
2020-01-17 16:06:27 +0900

    flont

---
+++ mnemwallet/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+  <ion-item>
+    <ion-label position="floating">password</ion-label>
+    <ion-input></ion-input>
+  </ion-item>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
